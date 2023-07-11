d087b8368b9510b1ae1e0b19249587ca1a4bc70e
Pedro Henrique Alves Veloso <86307207+PedroHAVeloso@users.noreply.github.com>
2023-07-11 17:51:43 -0300

    Exibição de frases completa.

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:vz_app/interactor/cubits/phrases/phrases_cubit.dart';
+import 'package:vz_app/ui/constants/app_colors.dart';
+import 'package:vz_app/ui/constants/app_icons.dart';
+import 'package:vz_app/ui/constants/app_theme.dart';
 import 'package:vz_app/ui/widgets/appbar_widget.dart';
 import 'package:vz_app/ui/widgets/bottom_appbar_widget.dart';
 
@@ -16,23 +21,114 @@ class HomePage extends StatelessWidget {
       appBar: AppBarWidget(
         scaffoldKey: scaffoldKey,
       ),
-      body: BlocBuilder<PhrasesCubit, PhrasesState>(
-        builder: (context, state) {
-          if (state is PhrasesInitial) {
-            return Container();
-          }
-          if (state is PhrasesLoading) {
-            return CircularProgressIndicator();
-          }
-          if (state is PhrasesLoaded) {
-            return Column(
-              children: [
-                Text(state.phrases.toString()),
-              ],
-            );
-          }
-          return Container();
-        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: BlocBuilder<PhrasesCubit, PhrasesState>(
+          builder: (context, state) {
+            if (state is PhrasesInitial) {
+              return Center(
+                child: RichText(
+                  text: const TextSpan(
+                    text: "Gere frases aleatórias de ",
+                    style: TextStyle(
+                      color: AppTheme.secondaryColor,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "velha do zap",
+                        style: TextStyle(
+                          color: AppTheme.accentColor,
+                        ),
+                      ),
+                      TextSpan(text: "."),
+                    ],
+                  ),
+                ),
+              );
+            }
+            if (state is PhrasesLoading) {
+              return const Center(
+                child: SizedBox(
+                  height: 60.0,
+                  width: 60.0,
+                  child: CircularProgressIndicator(
+                    color: AppColors.blue,
+                    strokeWidth: 5.0,
+                  ),
+                ),
+              );
+            }
+            if (state is PhrasesLoaded) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: state.phrases.length,
+                      itemBuilder: (context, items) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    InkWell(
+                                      onTap: () => Clipboard.setData(
+                                        ClipboardData(
+                                          text: state.phrases[items],
+                                        ),
+                                      ),
+                                      child: SvgPicture.asset(
+                                        AppIcons.copy,
+                                        colorFilter: const ColorFilter.mode(
+                                          AppTheme.accentColor,
+                                          BlendMode.srcIn,
+                                        ),
+                                        width: 24.0,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10.0),
+                                    SvgPicture.asset(
+                                      AppIcons.favorite,
+                                      colorFilter: const ColorFilter.mode(
+                                        AppTheme.accentColor,
+                                        BlendMode.srcIn,
+                                      ),
+                                      width: 24.0,
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(width: 20.0),
+                                Expanded(
+                                  child: Text(
+                                    state.phrases[items],
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.3,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Divider(
+                              thickness: 2.0,
+                              height: 30.0,
+                              color: AppTheme.tertiaryColor,
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }
+            return const SizedBox();
+          },
+        ),
       ),
       bottomNavigationBar: const BottomAppBarWidget(),
       endDrawer: const Drawer(),
