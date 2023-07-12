650f7ba8ad8a91d09d8457e2ed7abf6da84923fc
Pedro Henrique Alves Veloso <86307207+PedroHAVeloso@users.noreply.github.com>
2023-07-11 23:49:37 -0300

    Criação da constante apiUrl

--- lib/data/datasources/phrases_datasource.dart
+++ lib/data/datasources/phrases_datasource.dart
@@ -1,9 +1,11 @@
 import 'package:http/http.dart' as http;
 
 class PhrasesDatasource {
+  static const String apiUrl = "http://10.0.2.2:8080";
+
   Future<String> getPhrases({required int number}) async {
     final response = await http.get(
-      Uri.parse("http://10.0.2.2:8080/phrases/$number"),
+      Uri.parse("$apiUrl/phrases/$number"),
     );
 
     return response.body;
